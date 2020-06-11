c52868649c3bfbe1bf832fe1f0aefffba272aeae
Michael <michael_kirsch@t-online.de>
2020-06-11 21:18:00 +0200

    reupload

---
+++ ALGO_Infix_Prefix_Stuff/cmake-build-debug/CMakeFiles/ALGO_infix_prefix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ALGO_infix_prefix.exe"
+  "ALGO_infix_prefix.exe.manifest"
+  "ALGO_infix_prefix.pdb"
+  "CMakeFiles/ALGO_infix_prefix.dir/main.cpp.obj"
+  "libALGO_infix_prefix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ALGO_infix_prefix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
